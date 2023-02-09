5f5fd224f164c38324c19d5cd979286c5b1ab8a1
AruneSR <missarune@gmail.com>
2023-02-09 10:01:45 +0200

    add variables demo codes

---
+++ Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mano vardas: Arune");
+
+            string vardas = "Arune";
+            Console.Write("Mano vardas: ");
+            Console.WriteLine(vardas);
+
+            Console.WriteLine("Mano vardas: " + "Arune");
+            Console.WriteLine("Mano vardas: {0}, {1}", vardas, "S"); // galima pateigti tiesiog reiksme 
+
+            Console.WriteLine($"Mano vardas: {vardas}"); // geriausias budas
+
+            Console.WriteLine(); // tuscia eilute
+            Console.WriteLine("Nauja eilute galima  \n iterpti ir taip");
+
+        }
+    }
+}
---
+++ KIntamieji/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KIntamieji
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;// lietuviskos raides
+            Console.WriteLine("Labas Pasauli");
+
+            // Ši eilutė skirta tam , kad paleidimo proceso metu console liktu atidaryta//
+            Console.ReadKey();
+
+            string vardas = "Arune";
+            string kintamojoPavadinimas;
+
+            string pavarde = "SuRam";
+            int amzius = 34;
+            float ugis =  1.71;
+            double svoris = 56;
+            char simbolis = 'A'; 
+            bool tiesaMelas = false;
+            bool tusciasbool; // false
+            int tusciasint;// 0
+            string tusciasstring; // null
+
+            var kintamasis = 1; // kuriant kintamaji privaloma nurodyti tipa
+            kintamasis = 2; // veliau parasant  reiksme tipas nenurodomas
+
+
+
+
+
+        }
+    }
+}
