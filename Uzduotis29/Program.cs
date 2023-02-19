930299628925b254c6c65286a7a9c5b765c44778
AruneSR <missarune@gmail.com>
2023-02-19 18:59:36 +0200

    Uzduotis30 ir Uzduotis29

---
+++ Uzduotis29/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Uzduotis29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Parašykite metodą, kuris pasisveikintų su vartotoju.
+            // Vartotojas turi įvesti savo vardą, amžių ir hobio pavadinimą.
+            //  Visi šie duomenys yra duodami metodui, kuris visą šią informaciją išveda formatuotai.
+
+            Console.WriteLine("Įveskite savo varda:");
+            string vardas = Console.ReadLine();
+            
+            Console.WriteLine("Įveskite savo amžių");
+            int amzius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hobio pavadinimas");
+            string pavadinimas = Console.ReadLine();
+            PasisveikinimasPanaudojantVarda(vardas, amzius, pavadinimas);
+
+        }
+
+        
+
+        internal static void PasisveikinimasPanaudojantVarda (string vardas, int amzius, string pavadinimas)
+        {
+            Console.WriteLine($"Labas,{vardas} {(amzius)}");
+            Console.WriteLine($"Jūsų įrašytas hobis:{pavadinimas}");
+        }
+    }   
+}
---
+++ Uzduotis30/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Uzduotis30
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //  Leiskite vartotojui įvesti du sveikuosius skaičius į programą.  Sukurkite ir panaudokite šiuos metodus:
+            //  Suma;
+            //   Skirtumas;
+            //  Sandauga;
+            //   Dalmuo;
+
+            Console.WriteLine("Įveskite du sveikuosius skaičius");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("{0} + {1} = {2}", skaicius1, skaicius2, skaicius1 + skaicius2);
+            Console.WriteLine("{0} - {1} = {2}", skaicius1, skaicius2, skaicius1 - skaicius2);
+            Console.WriteLine("{0} * {1} = {2}", skaicius1, skaicius2, skaicius1 * skaicius2);
+            Console.WriteLine("{0} / {1} = {2}", skaicius1, skaicius2, skaicius1 / skaicius2);
+        } 
+    }
+
+    internal class Calculator
+    {
+        internal static int Sum(int skaicius1, int skaicius2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
